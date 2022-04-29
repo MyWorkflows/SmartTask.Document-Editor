2f47ced67067a2dd456c1d8dcedf0edc239ea390
Enrico <enricowillemse.was@gmail.com>
2022-04-29 20:25:34 +0800

    Update

--- src/smart-task-component.vue
+++ src/smart-task-component.vue
@@ -1,19 +1,14 @@
-<script lang="ts">
+<script>
 import { defineComponent } from "vue";
-import CKEditor from '@ckeditor/ckeditor5-vue';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 export default /*#__PURE__*/ defineComponent({
   name: "SmartTaskComponent", // vue component name
-  components: { CKEditor, ClassicEditor },
+  components: { QuillEditor },
   props: ['componentState', 'componentStatus'],
   data() {
     return { 
-         editor: ClassicEditor,
-                editorData: '<p>Content of the editor.</p>',
-                editorConfig: {
-                    // The configuration of the editor.
-                },
       model: {
       content:""
     } };
@@ -29,13 +24,36 @@ export default /*#__PURE__*/ defineComponent({
       this.model = this.componentState;
     }
   },
-  setup() {},
+  setup() {
+     const editorOption = {
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
+            [{ header: 1 }, { header: 2 }],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+            [{ script: 'sub' }, { script: 'super' }],
+            [{ indent: '-1' }, { indent: '+1' }],
+            [{ direction: 'rtl' }],
+            [{ size: ['small', false, 'large', 'huge'] }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            [{ color: [] }, { background: [] }],
+            [{ font: [] }],
+            [{ align: [] }],
+            ['clean'],
+            ['link', 'image', 'video']
+          ]
+        }
+      };
+
+return {editorOption}
+  },
 });
 </script>
 
 <template>
- <div style="font-family: Avenir, sans-serif">
-  <CKEditor style="height:500px" :editor="editor" v-model="editorData" :config="editorConfig"></CKEditor>
+ <div>
+  <QuillEditor  style="min-height:300px;" :options="editorOption" theme="snow" />
   </div>
  
 </template>
