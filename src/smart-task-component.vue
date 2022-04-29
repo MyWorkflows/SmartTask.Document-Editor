8d9a676f74fd244bd76107f93eb9ad2d30d03c08
Enrico Willemse <enricowillemse.was@gmail.com>
2022-04-29 11:51:05 +0000

    Update

--- src/smart-task-component.vue
+++ src/smart-task-component.vue
@@ -1,12 +1,22 @@
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import CKEditor from '@ckeditor/ckeditor5-vue';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 export default /*#__PURE__*/ defineComponent({
   name: "SmartTaskComponent", // vue component name
+  components: { CKEditor, ClassicEditor },
   props: ['componentState', 'componentStatus'],
   data() {
-    return { model: {} };
+    return { 
+         editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>',
+                editorConfig: {
+                    // The configuration of the editor.
+                },
+      model: {
+      content:""
+    } };
   },
   computed: {},
   methods: {
@@ -24,16 +34,9 @@ export default /*#__PURE__*/ defineComponent({
 </script>
 
 <template>
-  <form>
-    <div class="my-5">
-      <button
-        type="button"
-        @click="submitForm"
-        class="btn btn-sm btn-primary"
-      >
-       Mark as in Progress
-      </button>
-    </div>
-  </form>
+ <div style="font-family: Avenir, sans-serif">
+  <CKEditor style="height:500px" :editor="editor" v-model="editorData" :config="editorConfig"></CKEditor>
+  </div>
+ 
 </template>
 
